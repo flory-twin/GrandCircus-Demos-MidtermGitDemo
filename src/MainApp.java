326f7a22510b1b8035df916fa6c7a0965bfe60b2
srbuck1111 <srbuck1111@gmail.com>
2019-10-31 11:00:17 -0400

    garbage commit

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,6 +4,9 @@ public class MainApp
    public static void main(String[] args) {
       System.out.println("My name is NOT Antonella.");
       
-      System.out.println("It is Halloween.");
+      System.out.println("It's Halloween!");
+      while (true) {
+    	  System.out.println("Oh no.");
+      }
    }
 }
